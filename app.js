ac08420e8552d874550630ab5f43a5b01d4bad27
nairarjun <nair.arjundasm@gmail.com>
2020-08-03 15:05:33 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const moment = require('moment');
+
+const today = moment();
+console.log(today.format());
+
+console.log(moment("2020-01-01", "YYYY-MM-DD").isValid()); // true
+console.log(moment("not-a-date", "YYYY-MM-DD").isValid()); // false
+
+console.log(
+    moment("2019 was a great year because I got married", "YYYY-MM-DD").isValid()
+  );
+  // Returns true because 2019 matches YYYY
+
+moment().add(7, 'days');    // adds 7 days to current date
+moment().add(7, 'months');  // adds 7 months to current date
+moment().add(7, 'years');   // adds 7 years to current date
+
+
+moment().subtract(7, 'days');   // subtracts 7 days to current date
+moment().subtract(7, 'months'); // subtracts 7 months to current date
+moment().subtract(7, 'years');  // subtracts 7 years to current date
+
+const today = moment();
+const nextWeek = today.add(7, 'days');
+console.log(nextWeek.format('dddd Do MMMM, YYYY'));
